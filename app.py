d8b740a3a26ea18a06397b22ffd2388868895a93
tomasmerlo1 <114004039+tomasmerlo1@users.noreply.github.com>
2023-02-15 04:13:47 +0000

    asd

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
-def index():
- return '<h1>Hola!<h1>'
- hasdqa
+    def index():
+    return '<h1>Hola!<h1>'
+
+@app.route("/usuarios")
+def devolver_usuarios():
+    return jsonify(usuarios)

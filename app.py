5106179a61ed4aadd25f105d29a4175641f6dcd3
sofia gonzalez <sofimacag@gmail.com>
2023-02-15 01:11:10 -0300

    app inicio

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, request, Response
+from http import HTTPStatus
+import json
+import urllib
+
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
 @app.route('/')
-def index():
+def home():
  return '<h1>Hola!<h1>'

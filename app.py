53918a145107aff1c5902f19860cae68fca2f5c5
tomasmerlo1 <114004039+tomasmerlo1@users.noreply.github.com>
2023-02-15 18:02:51 +0000

    todosSS

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ import requests
 
 app = Flask(__name__)
 
+with open("directores.json",encoding='utf-8') as directores.json:
+    directores=json.load(directores_json)
+directores=directores[0]['directores']
+
+with open("peliculas.json",encoding='utf-8') as peliculas.json:
+    peliculas=json.load(biblioteca_json) 
+peliculas=peliculas[0]['peliculas']
+
 with open("usuarios.json",encoding='utf-8') as usuarios.json:
     usuarios=json.load(usuarios.json)
 usuarios=usuarios[0]["usuarios"]

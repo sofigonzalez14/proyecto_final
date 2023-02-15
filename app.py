9bee32d5ff090a8088cda09fba0a2631784d6b31
Macarena Huarte <113394641+Macahuarte09@users.noreply.github.com>
2023-02-15 05:02:19 +0000

    borrar hola

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, jsonify, request, Response
 app = Flask(__name__)
 @app.route('/')
 
-def index():
-    return '<h1>Hola!<h1>'
-
 @app.route("/usuarios")
 def devolver_usuarios():
     return jsonify(usuarios)

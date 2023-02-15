1fc578c92693af16856676c715da340478181375
Macarena Huarte <113394641+Macahuarte09@users.noreply.github.com>
2023-02-15 05:03:51 +0000

    a

--- app.py
+++ app.py
@@ -3,10 +3,5 @@ app = Flask(__name__)
 @app.route('/')
 
 @app.route("/usuarios")
-<<<<<<< HEAD
     def devolver_usuarios():
     return jsonify("JSON/usuarios")
-=======
-def devolver_usuarios():
-    return jsonify('usuarios')
-

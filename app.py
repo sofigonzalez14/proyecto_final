c5e3b814ec52be37bbcc5cec7e0a936a9d2a9c33
Macarena Huarte <113394641+Macahuarte09@users.noreply.github.com>
2023-02-16 18:51:33 +0000

    flask

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from http import HTTPStatus
 import threading
 
 app = Flask(__name__)
-@app.route("/usuarios")
-def devolver_usuarios():
-   return jsonify('usuarios')
+
+with open("usuarios.json",encoding='utf-8') as usuarios_json:
+    usuarios=json.load(usuarios_json)
+usuarios=usuarios[0]['usuarios']

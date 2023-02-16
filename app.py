bb5f4a2f2f488039fe7bd9595b3cc6f9caa0e9f4
sofia gonzalez <sofimacag@gmail.com>
2023-02-16 16:10:59 -0300

    actualizacion

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ from http import HTTPStatus
 import threading
 
 app = Flask(__name__)
-@app.route("/usuarios")
-def devolver_usuarios():
-   return jsonify('usuarios')
+
+with open("peliculas.json",encoding='utf-8') as peliculas_json:
+    peliculas=json.load(peliculas_json) 
+peliculas=peliculas[0]['peliculas']
+
+with open("usuarios.json",encoding='utf-8') as usuarios_json:
+    usuarios=json.load(usuarios_json)
+usuarios=usuarios[0]['usuarios']
+
+with open("directores.json",encoding='utf-8') as directores_json:
+    directores=json.load(directores_json)
+directores=directores[0]['directores']
+
+print("SISTEMA DE ENTRADAS: CINE\n")
+

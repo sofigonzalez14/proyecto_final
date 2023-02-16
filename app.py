8cdfdc0abc3df9094a8b06f3020ea57c8f1274e2
Macarena Huarte <113394641+Macahuarte09@users.noreply.github.com>
2023-02-16 20:17:22 +0000

    pelicula

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ with open("directores.json",encoding='utf-8') as directores_json:
     directores=json.load(directores_json)
 directores=directores[0]['directores']
 
-with open("biblioteca.json",encoding='utf-8') as biblioteca_json:
+with open("pelicula.json",encoding='utf-8') as biblioteca_json:
     peliculas=json.load(biblioteca_json) 
 peliculas=peliculas[0]['peliculas']
 

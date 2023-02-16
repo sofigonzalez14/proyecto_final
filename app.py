fc3871d46c09b2b18007b9104782591ddcff15ad
sofia gonzalez <sofimacag@gmail.com>
2023-02-16 15:19:19 -0300

    a

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
 import json
 from flask import Flask, jsonify, Response, request
 from http import HTTPStatus
-import requests
+import threading
 
 app = Flask(__name__)
-
-with open("directores.json",encoding='utf-8') as directores.json:
-    directores=json.load(directores_json)
-directores=directores[0]['directores']
-
-with open("peliculas.json",encoding='utf-8') as peliculas.json:
-    peliculas=json.load(biblioteca_json) 
-peliculas=peliculas[0]['peliculas']
-
-with open("usuarios.json",encoding='utf-8') as usuarios.json:
-    usuarios=json.load(usuarios.json)
-usuarios=usuarios[0]['usuarios']
-
-print("CARTELERA\n")
-
-ultimas_peliculas_agregadas=[]
-@app.route("/json/usuarios")
-def home()
- 
+@app.route("/usuarios")
+def devolver_usuarios():
+   return jsonify('usuarios')

22cdd080bf38e46466ce766a6480024379c01246
sofia gonzalez <sofimacag@gmail.com>
2023-02-17 11:28:35 -0300

    qq

--- app.py
+++ app.py
@@ -115,3 +115,43 @@ def devolver_peliculas_director(id):
         return jsonify(lista)
     else:
         return Response("Este director no ha sido encontrado", status=HTTPStatus.NOT_FOUND)
+
+#------------AGREGAR UNA PELICULA------------------
+def existe_pelicula(titulo):
+    peliculas = pelicula["peliculas"]
+    for pelicula in peliculas:
+        if pelicula['titulo'].lower() == titulo.lower():
+            return pelicula
+    return False
+
+#---------------------------------------------------
+@app.route("/agregar/pelicula", methods=['POST'])
+def alta_pelicula():
+    if usuario_privado == True:
+        data = request.get_json()
+        # validar data que viene del pedido
+        # data.keys() >= {"usuario", "titulo"} retorna true si hay coincidencia
+        campos = {"titulo", "genero","director", "id", "sinopsis", "enlace", "comentario"}
+        if data.keys() < campos:
+            return jsonify("Faltan campos en el pedido"), HTTPStatus.BAD_REQUEST
+
+        if existe_pelicula(data["titulo"]):
+            return jsonify("La pelicula ya existe en la base de datos."), HTTPStatus.BAD_REQUEST
+        
+        pelicula_nueva = {
+            "id": id,
+            "titulo": data["titulo"],
+            "anio": data["anio"],
+            "genero": data["genero"],
+            "genero_sub": data["genero_sub"],
+            "id_director": data["id_director"],
+            "sinopsis": data["sinopsis"],
+            "imagen": data["imagen"],
+            "trailer": data["trailer"],
+            "subidapor": data["subidapor"],
+        }
+        peliculas["peliculas"].append(pelicula_nueva)
+        print("Se cargo una pelicula nueva")
+        return jsonify(pelicula_nueva), HTTPStatus.OK
+    else:
+        return jsonify("Usted no es un usuario registrado"), HTTPStatus.BAD_REQUEST
